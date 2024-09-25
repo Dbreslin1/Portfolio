241440527c5d514dba35749f32ec0437f7202f77
Dbreslin1 <146346597+Dbreslin1@users.noreply.github.com>
2024-09-25 20:29:16 +0100

    Create routes.js

---
+++ PCWebsite/router/routes.js
@@ -0,0 +1,14 @@
+function loadPage(component) {
+  return () => import(`@/components/${component}.vue`);
+}
+
+export default [
+  { path: '/', component: loadPage('Home') }, // Add a comma here
+  { path: '/Builder', component: loadPage('Builder') },
+  { path: '/Comparison', component: loadPage('Comparison') },
+  { path: '/Support', component: loadPage('Support') },
+  { path: '/PreBuild', component: loadPage('PreBuild') },
+  { path: '/Register', component: loadPage('Register')},
+  { path: '/Login', component: loadPage('Login')},
+  { path: '/HomePage', component: loadPage('HomePage')}
+]
